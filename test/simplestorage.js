1d13270db8f87ed3db3779a77bc010f2a695a19c
Tobias Leong <tobias.leong@zendesk.com>
2021-11-05 22:54:05 +0800

    Add tests for getBalance and request

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,6 +1,6 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
-contract("SimpleStorage", accounts => {
+contract("SimpleStorage", (accounts) => {
   it("...should store the value 89.", async () => {
     const simpleStorageInstance = await SimpleStorage.deployed();
 
@@ -11,5 +11,37 @@ contract("SimpleStorage", accounts => {
     const storedData = await simpleStorageInstance.get.call();
 
     assert.equal(storedData, 89, "The value 89 was not stored.");
+  });
+
+  it("...should get the value 100 after storing it", async () => {
+    const simpleStorageInstance = await SimpleStorage.deployed();
+
+    // Set value of 89
+    await simpleStorageInstance.set(100, { from: accounts[0] });
+
+    // Get stored value
+    const storedData = await simpleStorageInstance.get.call();
+
+    assert.equal(storedData, 100, "The value 100 was not stored.");
+  });
+
+  it("...should get the balance of 0 by default", async () => {
+    const simpleStorageInstance = await SimpleStorage.deployed();
+
+    // Get stored value
+    const balance = await simpleStorageInstance.getBalance();
+
+    assert.equal(balance, 0, "The default balance value of 0 was not returned");
+  });
+
+  it("...should get the balance of 100 after requesting for it", async () => {
+    const simpleStorageInstance = await SimpleStorage.deployed();
+
+    await simpleStorageInstance.request(100);
+
+    // Get stored value
+    const balance = await simpleStorageInstance.getBalance();
+
+    assert.equal(balance, 100, "The balance value of 100 was not returned");
   });
 });
